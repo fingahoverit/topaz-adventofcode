dd36dd0ba336092370991a2283c99d583344a97f
fingahoverit <fingahoverit@gmail.com>
2019-11-25 23:27:04 +0100

    add common resource loader - 2019

---
+++ 2019/java/adventofcode-2019/src/test/java/com/github/fingahoverit/topaz_adventofcode/year2019/common/ExpectedLoader.java
@@ -0,0 +1,38 @@
+package com.github.fingahoverit.topaz_adventofcode.year2019.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExpectedLoader {
+
+    private static final Logger LOGGER = Logger.getLogger(ExpectedLoader.class.getCanonicalName());
+    private static final String DEFAULT_INPUT_FOLDER_NAME = "expected";
+
+    private ExpectedLoader() {
+    }
+
+    public static String load(String dayIndex, String expectedFile) {
+
+        try {
+            Path path = Paths.get(
+                    Objects.requireNonNull(
+                            InputLoader.class.getClassLoader().getResource(dayIndex + File.separator
+                                    + DEFAULT_INPUT_FOLDER_NAME + File.separator + expectedFile)
+                    ).toURI());
+
+            return Files.readString(path);
+
+        } catch (URISyntaxException | IOException | NullPointerException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+
+        return "";
+    }
+}
