afeb44b460a4f60970b52aa906ff44ecd83f1684
fingahoverit <fingahoverit@gmail.com>
2019-11-25 23:26:43 +0100

    init gradle - 2019

---
+++ 2019/java/adventofcode-2019/build.gradle.kts
@@ -0,0 +1,27 @@
+
+plugins {
+    java
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+group = "com.github.fingahoverit.topaz_adventofcode.year2019"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
